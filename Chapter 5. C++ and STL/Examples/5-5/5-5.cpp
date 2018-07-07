5ce42f571fe1589f46050ee75137ea24a7ae3151
chenBright <1021774709@qq.com>
2018-07-07 22:02:15 +0800

    Debug 5-5 UVa 12096 修复运行出错的问题

--- Chapter 5. C++ and STL/Examples/5-5/5-5.cpp
+++ Chapter 5. C++ and STL/Examples/5-5/5-5.cpp
@@ -1,59 +1,55 @@
-// 题目  UVa 12096 ：https://cn.vjudge.net/problem/UVA-12096
-
-#include <iostream>
-#include <string>
+/**
+ * UVa12096 - The SetStack Computer
+ * https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=835&page=show_problem&problem=3248
+ */
+#include <cstdio>
 #include <set>
-#include <map>
-#include <vector>
 #include <stack>
+#include <map>
 #include <algorithm>
 using namespace std;
 
-#define ALL(x) x.begin(),x.end()
-#define INS(x) inserter(x,x.begin())
+#define ALL(x) x.begin(), x.end()
+#define INS(x) inserter(x, x.begin())
 
+int T, N, n = 0;
 typedef set<int> Set;
-map<Set, int> IDCache;
-vector<Set> SetCache;
+map<Set, int> Set2IDMap;
+map<int, Set> ID2SetMap;
+stack<int> st;
 
 int ID(Set x) {
-	if (IDCache.count(x)) {
-		return IDCache[x];
-	}
-	SetCache.push_back(x);
-	return IDCache[x] = SetCache.size() - 1;
+	if (Set2IDMap.count(x)) return Set2IDMap[x];
+	Set2IDMap[x] = n;
+	ID2SetMap[n] = x;
+	return n++;
 }
 
 int main() {
-	stack<int> s;
-	int n;
-	cin >> n;
-
-	for (int i = 0; i < n; ++i) {
-		string op;
-		cin >> op;
-		if (op[0] == 'P') {
-			s.push(ID(Set()));
-		} else if (op[0] == 'D') {
-			s.push(s.top());
-		} else {
-			Set x1 = SetCache[s.top()];
-			s.pop();
-			Set x2 = SetCache[s.top()];
-			s.pop();
-			Set x;
-			if (op[0] == 'U') {
-				set_union(ALL(x1), ALL(x2), INS(x));
+	scanf("%d", &T);
+	while (T--) {
+		scanf("%d", &N);
+		while (!st.empty()) st.pop();
+		while (N--) {
+			char op[12];
+			scanf("%s", op);
+			if (op[0] == 'P') st.push(ID(Set()));
+			else if (op[0] == 'D') st.push(st.top());
+			else {
+				Set x1 = ID2SetMap[st.top()]; st.pop();
+				Set x2 = ID2SetMap[st.top()]; st.pop();
+				Set x;
+				if (op[0] == 'U') set_union(ALL(x1), ALL(x2), INS(x));
+				else if (op[0] == 'I') set_intersection(ALL(x1), ALL(x2), INS(x));
+				else if (op[0] == 'A') {
+					x = x2;
+					x.insert(ID(x1));
+				}
+				st.push(ID(x));
 			}
-			if (op[0] == 'I') {
-				set_intersection(ALL(x1), ALL(x2), INS(x));
-			}
-			if (op[0] == 'A') {
-				x = x2;
-				x.insert(ID(x1));
-			}
-			s.push(ID(x));
+			printf("%lu\n", ID2SetMap[st.top()].size());
 		}
+		printf("***\n");
 	}
-	cout << SetCache[s.top()].size() << endl;
+	return 0;
 }
